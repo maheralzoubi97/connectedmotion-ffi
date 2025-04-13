89283b39ee404730bab531259df68bfc435e81e0
Maher Alzoubi <maher@connectedmotion.io>
2025-04-13 16:08:07 +0300

    clean

--- lib/ffi.dart
+++ lib/ffi.dart
@@ -6,17 +6,12 @@ import 'package:camera/camera.dart';
 import 'package:connectedmotion_ffi/functions/functions.dart';
 import 'package:ffi/ffi.dart';
 
-void bgra88882Jpg(Map<String, dynamic> data) {
-  final port = data['send_port'] as SendPort;
-  final cameraImage = data['cameraImage'] as CameraImage;
-  final index = data['index'];
-  final isolateTimeStamp = data['isolateTimeStamp'];
-  final newWidth = data['newWidth'] as int;
-  final newHeight = data['newHeight'] as int;
-
+Map<String, Uint8List>? _processBgra8888Image(
+    CameraImage cameraImage, int newWidth, int newHeight) {
   final s = cameraImage.planes[0].bytes.length;
   final p = malloc.allocate<Uint8>(4 * cameraImage.height * cameraImage.width);
   p.asTypedList(s).setRange(0, s, cameraImage.planes[0].bytes);
+
   final segBoundary =
       malloc.allocate<Int32>(cameraImage.height * cameraImage.width);
   final segBoundarySize = malloc.allocate<Int32>(1);
@@ -69,21 +64,18 @@ void bgra88882Jpg(Map<String, dynamic> data) {
     final originalImageBytes = jpegBuf.value.asTypedList(jpegSize.value);
     final resizedImageBytes =
         resizedJpegBuf.value.asTypedList(resizedJpegSize.value);
-
-    port.send({
-      "index": index,
-      "isolateTimeStamp": isolateTimeStamp,
+    return {
       "originalImage": originalImageBytes,
       "resizedImage": resizedImageBytes,
-    });
+    };
   } finally {
     malloc.free(p);
     malloc.free(segBoundary);
     malloc.free(segBoundarySize);
-    malloc.free(jpegBuf.value);
+    if (jpegBuf.value != nullptr) malloc.free(jpegBuf.value);
     malloc.free(jpegBuf);
     malloc.free(jpegSize);
-    malloc.free(resizedJpegBuf.value);
+    if (resizedJpegBuf.value != nullptr) malloc.free(resizedJpegBuf.value);
     malloc.free(resizedJpegBuf);
     malloc.free(resizedJpegSize);
   }
@@ -119,14 +111,8 @@ typedef YUV2JPG = void Function(
     int,
     int);
 
-void yuv2jpg(Map<String, dynamic> data) {
-  var port = data['send_port'] as SendPort;
-  var cameraImage = data['cameraImage'] as CameraImage;
-  var index = data['index'];
-  var isolateTimeStamp = data['isolateTimeStamp'];
-  var newWidth = data['newWidth'] as int;
-  var newHeight = data['newHeight'] as int;
-
+Map<String, Uint8List>? _processYuv420Image(
+    CameraImage cameraImage, int newWidth, int newHeight) {
   final yPlane = cameraImage.planes[0];
   final uPlane = cameraImage.planes[1];
   final vPlane = cameraImage.planes[2];
@@ -144,10 +130,10 @@ void yuv2jpg(Map<String, dynamic> data) {
   final resizedJpegBuf = malloc.allocate<Pointer<Uint8>>(1);
   final resizedJpegSize = malloc.allocate<Int32>(1);
 
-  final yuv2jpg = dylib.lookupFunction<YUV2JPGFunction, YUV2JPG>('YUV2JPG');
+  final yuv2jpgFunc = dylib.lookupFunction<YUV2JPGFunction, YUV2JPG>('YUV2JPG');
 
   try {
-    yuv2jpg(
+    yuv2jpgFunc(
       yData,
       uData,
       vData,
@@ -163,26 +149,76 @@ void yuv2jpg(Map<String, dynamic> data) {
       newHeight,
     );
 
-    final originalImageBytes =
-        originalJpegBuf.value.asTypedList(originalJpegSize.value);
-    final resizedImageBytes =
-        resizedJpegBuf.value.asTypedList(resizedJpegSize.value);
+    final originalImageBytes = Uint8List.fromList(
+        originalJpegBuf.value.asTypedList(originalJpegSize.value));
+    final resizedImageBytes = Uint8List.fromList(
+        resizedJpegBuf.value.asTypedList(resizedJpegSize.value));
 
-    port.send({
-      "index": index,
-      "isolateTimeStamp": isolateTimeStamp,
+    return {
       "originalImage": originalImageBytes,
       "resizedImage": resizedImageBytes,
-    });
+    };
   } finally {
     malloc.free(yData);
     malloc.free(uData);
     malloc.free(vData);
-    malloc.free(originalJpegBuf.value);
+    if (originalJpegBuf.value != nullptr) malloc.free(originalJpegBuf.value);
     malloc.free(originalJpegBuf);
     malloc.free(originalJpegSize);
-    malloc.free(resizedJpegBuf.value);
+    if (resizedJpegBuf.value != nullptr) malloc.free(resizedJpegBuf.value);
     malloc.free(resizedJpegBuf);
     malloc.free(resizedJpegSize);
   }
+}
+
+void processImageInIsolate(SendPort mainSendPort) {
+  final receivePort = ReceivePort();
+  // Send the port to receive messages on
+  mainSendPort.send(receivePort.sendPort);
+
+  receivePort.listen((message) {
+    if (message is Map<String, dynamic>) {
+      final responsePort = message['responsePort'] as SendPort;
+      final cameraImage = message['cameraImage'] as CameraImage;
+      final newWidth = message['newWidth'] as int;
+      final newHeight = message['newHeight'] as int;
+      final isAndroid = message['isAndroid'] as bool;
+
+      final stopwatch = Stopwatch()..start();
+      try {
+        Map<String, Uint8List>? result;
+
+        if (isAndroid) {
+          // Process Android YUV image
+          result = _processYuv420Image(cameraImage, newWidth, newHeight);
+        } else {
+          // Process iOS BGRA image
+          result = _processBgra8888Image(cameraImage, newWidth, newHeight);
+        }
+
+        if (result != null) {
+          responsePort.send({
+            'success': true,
+            'originalImage': result['originalImage'],
+            'resizedImage': result['resizedImage'],
+            'processingTimeMs': stopwatch.elapsedMilliseconds,
+          });
+        } else {
+          responsePort.send({
+            'success': false,
+            'error': 'Image processing failed',
+            'processingTimeMs': stopwatch.elapsedMilliseconds,
+          });
+        }
+      } catch (e) {
+        responsePort.send({
+          'success': false,
+          'error': e.toString(),
+          'processingTimeMs': stopwatch.elapsedMilliseconds,
+        });
+      } finally {
+        stopwatch.stop();
+      }
+    }
+  });
 }
